6db1a676ab1c2421e79b7120eb98bc8706063cfd
Khai Truong <kd.kidr@gmail.com>
2020-11-23 18:03:47 +0700

    additive scene loader

---
+++ Assets/Scripts/AdditiveSceneLoader.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class AdditiveSceneLoader : MonoBehaviour
+{
+    [SerializeField]
+    private string sceneName; 
+    private void Start()
+    {
+        SceneManager.LoadSceneAsync(sceneName, LoadSceneMode.Additive);
+    }
+
+}
