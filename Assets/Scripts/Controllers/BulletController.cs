a833b912b7e2639d48791b46208052314e411718
Khai Truong <kd.kidr@gmail.com>
2021-03-19 23:41:10 +0700

    Fixed bullet rotation

--- Assets/Scripts/Controllers/BulletController.cs
+++ Assets/Scripts/Controllers/BulletController.cs
@@ -13,6 +13,7 @@ public class BulletController : MonoBehaviour
     private int currentBulletCount = 0;
     [SerializeField] private int bulletCap = 20;
     [SerializeField] private Vector3 offset;
+    [SerializeField] private float bulletScaleFactor = 0.3f;
     private void Awake()
     {
         cam = Camera.main;
@@ -48,14 +49,23 @@ public class BulletController : MonoBehaviour
         {
             Bullet bullet = bullets[i];
             Transform bulletTransform = bulletContainer.GetChild(i);
-            Vector3 head = new Vector3(bullet.x1 + bullet.x2, bullet.y1 + bullet.y2) / 2;
-            Vector3 tail = new Vector3(bullet.x3 + bullet.x4, bullet.y3 + bullet.y4) / 2;
+            Vector3 topLeft = cam.ScreenToWorldPoint(new Vector3(bullet.x1, Screen.height - bullet.y1) + offset);
+            Vector3 topRight = cam.ScreenToWorldPoint(new Vector3(bullet.x2, Screen.height - bullet.y2) + offset);
+            Vector3 bottomRight = cam.ScreenToWorldPoint(new Vector3(bullet.x3, Screen.height - bullet.y3) + offset);
+            Vector3 bottomLeft = cam.ScreenToWorldPoint(new Vector3(bullet.x4, Screen.height - bullet.y4) + offset);
+            Debug.DrawLine(topLeft, topRight, Color.red);
+            Debug.DrawLine(topRight, bottomRight, Color.green);
+            Debug.DrawLine(bottomRight, bottomLeft);
+            Debug.DrawLine(bottomLeft, topLeft);
+            Vector3 head = (topLeft + topRight) / 2;
+            Vector3 tail = (bottomLeft + bottomRight) / 2;
             Vector3 diff = head - tail;
             Vector3 center = (head + tail) / 2;
             float size = diff.magnitude;
             float angle = Mathf.Atan2(diff.y, diff.x) * Mathf.Rad2Deg;
-            bulletTransform.position = cam.ScreenToWorldPoint(center + offset);
-            bulletTransform.rotation = Quaternion.Euler(0, angle, 0);
+            bulletTransform.position = center;
+            bulletTransform.rotation = Quaternion.Euler(-angle, 90, 0);
+            bulletTransform.localScale = new Vector3(1, 1, 1) * size * bulletScaleFactor;
         }
     }
 
